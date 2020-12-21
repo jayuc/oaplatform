3f76e61d39527746cf7923be403229bc52fb0381
余杰 <402842327@qq.com>
2020-12-21 12:18:30 +0800

    正在加载样式调整

--- webFront/src/utils/LoadingUtil.js
+++ webFront/src/utils/LoadingUtil.js
@@ -4,7 +4,10 @@ import { Loading } from 'element-ui';
 // 开始正在加载
 const start = (_options) => {
     let options = {
-        fullscreen: true
+        fullscreen: true,
+        background: 'rgba(0, 0, 0, 0.8)',
+        spinner: 'el-icon-loading',
+        text: '加载中'
     };
     Object.assign(options, _options);
     return Loading.service(options);
