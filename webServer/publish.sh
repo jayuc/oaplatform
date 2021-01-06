e75485be3d8ba9c0b447969e29705a22f40b4afe
余杰 <402842327@qq.com>
2021-01-06 21:21:19 +0800

    修复问题

--- webServer/publish.sh
+++ webServer/publish.sh
@@ -4,19 +4,9 @@
 
 echo "process start ... "
 
-echo "setp 1/5: kill pidfile.txt"
-if [ -e ./pidfile.txt ]; then
-  kill -9 `cat pidfile.txt`
-fi
-
-echo "setp 2/5: kill nohup.out"
-if [ -e ./nohup.out ]; then
-  rm ./nohup.out
-fi
-
 cd ../
 
-echo "setp 3/5: git pull"
+echo "setp 1/5: git pull"
 git pull > pullResult.text
 
 # 如果没有代码更新，则不部署
@@ -26,9 +16,21 @@ if [ `echo $pullR | sed s/[[:space:]]//g` = "Alreadyuptodate." ];then
 	exit 0
 fi
 
+cd ./webServer/
+
+echo "setp 2/5: kill pidfile.txt"
+if [ -e ./pidfile.txt ]; then
+  kill -9 `cat pidfile.txt`
+fi
+
+echo "setp 3/5: kill nohup.out"
+if [ -e ./nohup.out ]; then
+  rm ./nohup.out
+fi
+
 # 编译项目
 echo "setp 4/5: mvn package"
-cd ./webServer/
+
 mvn clean package -Dmaven.test.skip=true
 
 echo "setp 5/5: run jar"
