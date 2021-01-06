01d6730d7541fc8accc6c0222988c031e9916b61
余杰 <402842327@qq.com>
2021-01-06 16:29:10 +0800

    提交

--- webServer/publish.sh
+++ webServer/publish.sh
@@ -4,22 +4,22 @@
 
 echo "process start ... "
 
-echo "setp 1/5: kill pidfile.txt"
+echo "setp 1/4: kill pidfile.txt"
 if [ -e ./pidfile.txt ]; then
   kill -9 `cat pidfile.txt`
 fi
 
 cd ../
 
-echo "setp 2/5: git push"
+echo "setp 2/4: git push"
 git pull
 
 # 编译项目
-echo "setp 3/5: mvn package"
+echo "setp 3/4: mvn package"
 cd ./webServer/
 mvn clean package -Dmaven.test.skip=true
 
-echo "setp 4/5: run jar"
+echo "setp 4/4: run jar"
 nohup java -jar ./target/oa-0.0.1.jar & echo $! > pidfile.txt
 
 echo "finshed."
