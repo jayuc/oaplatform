fb4ef8561c4e55b26e0e03f447c26aace48275d1
余杰 <402842327@qq.com>
2021-01-06 22:05:15 +0800

    自动部署脚本

--- webServer/publish.sh
+++ webServer/publish.sh
@@ -2,38 +2,38 @@
 
 #set -eo pipefail
 
-echo "process start ... "
+echo "process start ... " >> ./nohup.out
 
 cd ../
 
-echo "setp 1/5: git pull"
+echo "setp 1/5: git pull" >> ./nohup.out
 git pull > pullResult.text
 
 # 如果没有代码更新，则不部署
 pullR=`cat pullResult.text`
 if [ `echo $pullR | sed s/[[:space:]]//g` = "Alreadyuptodate." ];then
-    echo "Already up to date."
+    echo "Already up to date." >> ./nohup.out
 	exit 0
 fi
 
 cd ./webServer/
 
-echo "setp 2/5: kill pidfile.txt"
+echo "setp 2/5: kill pidfile.txt" >> ./nohup.out
 if [ -e ./pidfile.txt ]; then
   kill -9 `cat pidfile.txt`
 fi
 
-echo "setp 3/5: kill nohup.out"
+echo "setp 3/5: kill nohup.out" >> ./nohup.out
 if [ -e ./nohup.out ]; then
   rm ./nohup.out
 fi
 
 # 编译项目
-echo "setp 4/5: mvn package"
+echo "setp 4/5: mvn package" >> ./nohup.out
 
 mvn clean package -Dmaven.test.skip=true
 
-echo "setp 5/5: run jar"
+echo "setp 5/5: run jar" >> ./nohup.out
 nohup java -jar ./target/oa-0.0.1.jar & echo $! > pidfile.txt
 
-echo "finshed."
+echo "finshed." >> ./nohup.out
