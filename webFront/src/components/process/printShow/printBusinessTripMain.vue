1db462dde8827f7312e57b8622b50134712cdd1c
余杰 <402842327@qq.com>
2021-01-06 11:28:32 +0800

    申请单打印

--- webFront/src/components/process/printShow/printBusinessTripMain.vue
+++ webFront/src/components/process/printShow/printBusinessTripMain.vue
@@ -7,46 +7,63 @@
         <div ref="tableContainer">
             <table border="1" cellspacing="0" align="center"
                    style="font-family:微软雅黑;text-align:center;word-wrap:break-word;word-break:break-all;background:#efefef;color:#333;font-size:13px;border-collapse:collapse;width:100%">
-                <caption align="top"><h2 style="font-size:18px;">出差审批单</h2></caption>
+                <caption align="top"><h2 style="font-size:18px;">合肥市烟草专卖局（公司）<br/>出差审批单</h2></caption>
                 <tr height="65">
-                    <th width="35">申请单位（部门）</th>
-                    <th width="35" colspan="2">{{formatOrg(item.applyOrgId)}}</th>
+                    <th width="15%">申请人单位（部门）</th>
+                    <td width="40%">{{formatOrg(item.applyOrgId)}}</td>
+                    <th width="15%">申请人</th>
+                    <td colspan="24">{{item.applyName}}</td>
                 </tr>
                 <tr height="65">
-                    <th width="35">出差事由</th>
-                    <th width="30" colspan="2">{{item.content}}</th>
+                    <th colspan="1">出差事由</th>
+                    <td colspan="23">{{item.content}}</td>
                 </tr>
                 <tr height="65">
-                    <th width="35">出差人员及人数</th>
-                    <th width="35" colspan="2" style="position: relative;">
-                        <span>{{item.applyName}}</span>
-                        <em style="position: absolute;bottom: 5px;right: 20px;">共计1人</em>
-                    </th>
+                    <th colspan="1">出差人员</th>
+                    <td colspan="1">{{item.peopleList}}</td>
+                    <th colspan="1">人数</th>
+                    <td colspan="1">{{item.peopleNumber}}人</td>
                 </tr>
                 <tr height="65">
-                    <th width="35">出差目的地</th>
-                    <th width="30" colspan="2">{{item.address}}</th>
+                    <th colspan="1">出差目的地</th>
+                    <td colspan="1">{{item.address}}</td>
+                    <th colspan="1">预计出差时间</th>
+                    <td colspan="1">{{timeCg(item.startTime)}} ~ {{timeCg(item.endTime)}}</td>
                 </tr>
-                <tr height="65">
-                    <th width="35">预计出差时间</th>
-                    <th width="30" colspan="2">自{{timeCg(item.startTime)}} 至 {{timeCg(item.endTime)}}</th>
+                <tr height="65" v-if="l5">
+                    <th colspan="1">部门负责人意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l5map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l5map)}}</em>
+                    </td>
                 </tr>
-                <tr height="95">
-                    <th width="35" style="position: relative;">
-                        <span style="position: absolute;top: 5px;left: 20px;">申请人签字：</span>
-                        <i></i>
-                        <em style="position: absolute;bottom: 5px;right: 20px;"></em>
-                    </th>
-                    <th width="35" style="position: relative;">
-                        <span style="position: absolute;top: 5px;left: 20px;">单位（部门）审批：</span>
-                        <i></i>
-                        <em style="position: absolute;bottom: 5px;right: 20px;"></em>
-                    </th>
-                    <th width="35" style="position: relative;">
-                        <span style="position: absolute;top: 5px;left: 20px;">市局（公司）领导审批：</span>
-                        <i></i>
-                        <em style="position: absolute;bottom: 5px;right: 20px;"></em>
-                    </th>
+                <tr height="65" v-if="l4">
+                    <th colspan="1">单位（部门）分管领导意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l4map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l4map)}}</em>
+                    </td>
+                </tr>
+                <tr height="65" v-if="l3">
+                    <th colspan="1">单位（部门）负责人意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l3map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l3map)}}</em>
+                    </td>
+                </tr>
+                <tr height="65" v-if="l2">
+                    <th colspan="1">市局（公司）分管领导意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l2map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l2map)}}</em>
+                    </td>
+                </tr>
+                <tr height="65" v-if="l1">
+                    <th colspan="1">市局（公司）主要领导意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l1map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l1map)}}</em>
+                    </td>
                 </tr>
             </table>
         </div>
@@ -61,18 +78,58 @@
 <script>
     import moment from 'moment';
     import OrgUtil from '@/utils/OrgUtil';
+    import RestUtil from '@/utils/RestUtil';
     export default {
         name: "printBusinessTripMain",
         data() {
             return {
                 visible: false,
-                item: {}
+                item: {},
+                l5: false,
+                l4: false,
+                l3: false,
+                l2: false,
+                l1: false,
+                l5map: {},
+                l4map: {},
+                l3map: {},
+                l2map: {},
+                l1map: {}
             }
         },
         methods: {
             open(row) {
                 this.item = row;
                 this.visible = true;
+                RestUtil.get('oa/bill/opera/listAll',
+                    {billCode: row.billCode}).then((list) => {
+                    if(list instanceof Array){
+                        let stepMap = {};
+                        for (let i=0; i<list.length; i++){
+                            let item = list[i];
+                            let step = item.billStep;
+                            if(step != '00' && !stepMap[step]){
+                                if(item.stepOrgLevel == 1){
+                                    this.l1 = true;
+                                    this.l1map = item;
+                                }else if(item.stepOrgLevel == 2){
+                                    this.l2 = true;
+                                    this.l2map = item;
+                                }else if(item.stepOrgLevel == 3){
+                                    this.l3 = true;
+                                    this.l3map = item;
+                                }else if(item.stepOrgLevel == 4){
+                                    this.l4 = true;
+                                    this.l4map = item;
+                                }else if(item.stepOrgLevel == 5){
+                                    this.l5 = true;
+                                    this.l5map = item;
+                                }
+                            }
+                            stepMap[step] = step;
+                        }
+                    }
+                });
             },
             //打印表格
             print() {
@@ -93,6 +150,18 @@
             },
             timeCg(time) {
                 return moment(time).format('YYYY年MM月DD日');
+            },
+            handleContent(obj){
+                if(obj){
+                    return obj.content;
+                }
+                return '';
+            },
+            handleApproveTime(obj){
+                if(obj){
+                    return obj.createTime;
+                }
+                return '';
             }
         }
     }
--- webFront/src/components/process/printShow/printLeave.vue
+++ webFront/src/components/process/printShow/printLeave.vue
@@ -9,53 +9,57 @@
                    style="font-family:微软雅黑;text-align:center;word-wrap:break-word;word-break:break-all;background:#efefef;color:#333;font-size:13px;border-collapse:collapse;width:100%">
                 <caption align="top"><h2 style="font-size:18px;">合肥市烟草专卖局（公司）<br/>年休假申请表</h2></caption>
                 <tr height="65">
-                    <th width="35" colspan="2">单位（部门）</th>
-                    <th width="35" colspan="6">{{formatOrg(item.applyOrgId)}}</th>
+                    <th width="15%">申请人单位（部门）</th>
+                    <td width="40%">{{formatOrg(item.applyOrgId)}}</td>
+                    <th width="15%">申请人</th>
+                    <td colspan="24">{{item.applyName}}</td>
                 </tr>
                 <tr height="65">
-                    <th width="35">申请人</th>
-                    <th width="30">{{item.applyName}}</th>
-                    <th width="35">工龄（年）</th>
-                    <th width="20">{{item.workAge}}</th>
-                    <th width="35">休假标准</th>
-                    <th width="55">{{handleHolidayType(item.holidayType)}}</th>
-                    <th width="35">休假时间</th>
-                    <th width="65">{{timeCg(item.startTime)}} ~ {{timeCg(item.endTime)}}</th>
+                    <th colspan="1">工龄（年）</th>
+                    <td colspan="1">{{item.workAge}}</td>
+                    <th colspan="1">休假标准</th>
+                    <td colspan="1">{{handleHolidayType(item.holidayType)}}</td>
                 </tr>
-                <tr height="65" v-if="levelShowMap[5]">
-                    <th width="35" colspan="3">部门负责人意见</th>
-                    <th width="35" colspan="5" style="position: relative;">
-                        <span style="color: red;font-weight: bold">{{handleContent(levelShowMap[5])}}</span>
-                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(levelShowMap[5])}}</em>
-                    </th>
+                <tr height="65">
+                    <th colspan="1">休假时间</th>
+                    <td colspan="1">{{timeCg(item.startTime)}} ~ {{timeCg(item.endTime)}}</td>
+                    <th colspan="1">休假天数</th>
+                    <td colspan="1">{{item.days}}天</td>
                 </tr>
-                <tr height="65" v-if="levelShowMap[4]">
-                    <th width="35" colspan="3">单位（部门）分管领导意见</th>
-                    <th width="35" colspan="5" style="position: relative;">
-                        <span style="color: red;font-weight: bold">{{handleContent(levelShowMap[4])}}</span>
-                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(levelShowMap[4])}}</em>
-                    </th>
+                <tr height="65" v-if="l5">
+                    <th colspan="1">部门负责人意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l5map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l5map)}}</em>
+                    </td>
                 </tr>
-                <tr height="65" v-if="levelShowMap[3]">
-                    <th width="35" colspan="3">单位（部门）负责人意见</th>
-                    <th width="35" colspan="5" style="position: relative;">
-                        <span style="color: red;font-weight: bold">{{handleContent(levelShowMap[3])}}</span>
-                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(levelShowMap[3])}}</em>
-                    </th>
+                <tr height="65" v-if="l4">
+                    <th colspan="1">单位（部门）分管领导意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l4map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l4map)}}</em>
+                    </td>
                 </tr>
-                <tr height="65" v-if="levelShowMap[2]">
-                    <th width="35" colspan="3">市局（公司）分管领导意见</th>
-                    <th width="35" colspan="5" style="position: relative;">
-                        <span style="color: red;font-weight: bold">{{handleContent(levelShowMap[2])}}</span>
-                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(levelShowMap[2])}}</em>
-                    </th>
+                <tr height="65" v-if="l3">
+                    <th colspan="1">单位（部门）负责人意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l3map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l3map)}}</em>
+                    </td>
                 </tr>
-                <tr height="65" v-if="levelShowMap[1]">
-                    <th width="35" colspan="3">市局（公司）主要领导意见</th>
-                    <th width="35" colspan="5" style="position: relative;">
-                        <span style="color: red;font-weight: bold">{{handleContent(levelShowMap[1])}}</span>
-                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(levelShowMap[1])}}</em>
-                    </th>
+                <tr height="65" v-if="l2">
+                    <th colspan="1">市局（公司）分管领导意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l2map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l2map)}}</em>
+                    </td>
+                </tr>
+                <tr height="65" v-if="l1">
+                    <th colspan="1">市局（公司）主要领导意见</th>
+                    <td colspan="23" style="position: relative;">
+                        <span style="color: red;font-weight: bold">{{handleContent(l1map)}}</span>
+                        <em style="position: absolute;bottom: 5px;right: 20px;">{{handleApproveTime(l1map)}}</em>
+                    </td>
                 </tr>
             </table>
         </div>
@@ -78,26 +82,46 @@
             return {
                 visible: false,
                 item: {},
-                levelShowMap: {
-                    3: {}
-                }
+                l5: false,
+                l4: false,
+                l3: false,
+                l2: false,
+                l1: false,
+                l5map: {},
+                l4map: {},
+                l3map: {},
+                l2map: {},
+                l1map: {}
             }
         },
         methods: {
             open(row) {
                 this.item = row;
                 this.visible = true;
-                RestUtil.get('oa/bill/opera/listAll', {billCode: row.billCode}).then((list) => {
+                RestUtil.get('oa/bill/opera/listAll',
+                    {billCode: row.billCode}).then((list) => {
                     if(list instanceof Array){
                         let stepMap = {};
                         for (let i=0; i<list.length; i++){
                             let item = list[i];
                             let step = item.billStep;
                             if(step != '00' && !stepMap[step]){
-                                let t = {};
-                                t.content = item.content;
-                                t.approveTime = item.createTime;
-                                this.levelShowMap[item.stepOrgLevel] = t;
+                                if(item.stepOrgLevel == 1){
+                                    this.l1 = true;
+                                    this.l1map = item;
+                                }else if(item.stepOrgLevel == 2){
+                                    this.l2 = true;
+                                    this.l2map = item;
+                                }else if(item.stepOrgLevel == 3){
+                                    this.l3 = true;
+                                    this.l3map = item;
+                                }else if(item.stepOrgLevel == 4){
+                                    this.l4 = true;
+                                    this.l4map = item;
+                                }else if(item.stepOrgLevel == 5){
+                                    this.l5 = true;
+                                    this.l5map = item;
+                                }
                             }
                             stepMap[step] = step;
                         }
@@ -135,7 +159,7 @@
             },
             handleApproveTime(obj){
                 if(obj){
-                    return obj.approveTime;
+                    return obj.createTime;
                 }
                 return '';
             }
