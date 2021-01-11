286c3c13ba8963d76b1fb18daf4442cc21c73231
余杰 <402842327@qq.com>
2021-01-11 11:50:06 +0800

    脚本更新

---
+++ webServer/db/update20210111.sql
@@ -0,0 +1,13 @@
+#
+# 2020年12月25日需要执行的数据库脚本
+# 余杰 create by 2020/12/25
+
+CREATE database if NOT EXISTS `oa` default character set utf8mb4 collate utf8mb4_unicode_ci;
+use `oa`;
+
+SET NAMES utf8mb4;
+
+select * from t_oa_bill_opera where pass_flag = 1 and content is null;
+
+update  t_oa_bill_opera set content = '同意' where pass_flag = 1 and content is null
+
